4c8ac4e64460fb1a72842d219e2dbbaf3ecc21b0
Gaab <vieirabiel2@hotmail.com>
2025-05-29 08:51:58 -0300

    Update 5-Calculo_IMC.py

--- 5-Calculo_IMC.py
+++ 5-Calculo_IMC.py
@@ -7,7 +7,7 @@ if imc<18.5:
     print("Classificação: Abaixo do peso")
 elif imc>=18.5 and imc<=24.9:
     print("Classificação: Peso normal")
-elif imc>=25.0 and imc<+29.9:
+elif imc>=25.0 and imc<=29.9:
     print("Classificação: Sobrepeso")
 elif imc>=30.0:
     print("Classificação: Obesidade")
